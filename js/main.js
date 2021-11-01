b976603f73f422aafa76d57ba08c09fa124ba56a
David-Mandizera <david.mandizera@uncommon.org>
2021-11-01 14:49:22 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+window.onload=function(){
+    const menu_btn=document.querySelector('.hamburger');
+    const mobile_menu= document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+   
+}
+
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+
+
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+var blue = document.getElementById("blue");
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('show');
+  blue.classList.toggle('slide');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
